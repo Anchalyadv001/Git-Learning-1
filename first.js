fa573a183ad715e84c9cc8c7e3fa666d3db4e9a6
Anchalyadv001 <anchalyadav9569@gmail.com>
2026-09-10 10:35:01 +0530

    added index.js

--- first.js
+++ first.js
@@ -1,17 +1,18 @@
-const name="anchal";
-const age=19;
-const balance=420;
-console.log("hello words");
+function timing(){
+const time= document.getElementById("root");
+const now= new Date();
+const indiantime=now.toLocaleTimeString();
+time.innerHTML=indiantime;
+};
+
+setInterval(timing,1000);
+
+const time= document.getElementById("root");
+time.style.fontSize="200px";
+time.style.display="flex";
+time.style.justifyContent="center";
+time.style.alignItems="center";
+time.style.height="100vh";
 
 
-console.log("money honey");
-const a=12;
-const b=13;
-console.log(a+b);
 
-function showage(){
-    console.log(age);
-}
-showage();
-
-console.log("hello coder army");
